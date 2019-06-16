73601940a823e9d285fa98308a4d62b729c2763d
nilam1996 <nilam18@navgurukul.org>
2019-06-16 11:10:16 +0530

    till exists compl

--- meradb.py
+++ meradb.py
@@ -1,4 +1,4 @@
-import json
+import json,os
 def load(fileName='hello.db'):
     meraDB = MeraDB(fileName)
     meraDB.load_file()
@@ -13,21 +13,62 @@ class MeraDB():
         self.fileName = fileName
 
     def load_file(self):
-        print "Loading Database from ", self.fileName, " !"
-        content = open(self.fileName).read()
-        if content=="":
+        print ("Loading Database from ", self.fileName, " !")
+        if os.path.exists(self.fileName):
+        #If the file is exists then red either go to else and write data in fileName
+
+            file = open(self.fileName,'r')
+        else:
+            file = open(self.fileName,'w')    
+        data = file.read()
+        #if there is no data then dump whole data and if there is data then go to else parts and do loads data in jObject
+        if data=="":
             self.dump()
         else:
-            self.jObject = json.loads(content)
-        print "DB loaded successfully!"
+            self.jObject = json.loads(data)
+        print ("DB loaded successfully!")
         return self.jObject
 
     def dump(self):
-        print "Dumping database to ", self.fileName, " !"
+        print ("Dumping database to ", self.fileName, " !")
         file_handler = open(self.fileName, 'w')
         json_dump = json.dumps(self.jObject)
         file_handler.write(json_dump)
         file_handler.close()
 
-        print "DB dumped successfully!"
+        print ("DB dumped successfully!")
         return "OK"
+    def set(self,key,value):
+        self.jObject[key]=value
+        return True
+    def get(self,key):
+        try:
+            print (self.jObject[key])
+            return self.jObject[key]
+
+        except: 
+            print ("there is no key " + key)  
+            return False
+    def get_all(self):
+        list=[]
+        for key in  self.jObject:
+            list.append(key)
+        print (list)
+        return list
+    def rem(self,key):
+        try:
+            del self.jObject[key]
+            return True
+        except:
+            print ("there is no key " + key)
+            return False
+    def exists(self,key):
+        if key in self.jObject:
+            print (True)
+            return True
+        else:
+            print (False)
+            return False
+
+        
+
--- usedb.py
+++ usedb.py
@@ -1,3 +1,12 @@
 import meradb
-db=meradb.load("table.db")
+db=meradb.load("tab.db")
+# db.set("name","nilam")
+#db.set("class",150)
+# db.set("address","bangalore")
+db.get("name")
+db.get("nilam")
+db.get("class")
+db.get_all()
+db.rem("address")
+db.exists("name")
 db.dump()
